f42d46d544f0b004c81517b412bd614459f8b7b0
azraksk <yusraazrademirel@gmail.com>
2026-05-30 13:57:41 +0300

    Remove flat referans copy before importing full git history.

--- 15-smart-city-energy-trade/referans/decentralized-energy-trading/netting-entity/README.md
+++
@@ -1,6 +0,0 @@
-# NED-Server
-Run NED-Server with
-```
-yarn run-netting-entity
-```
-Config can be set in `root/ned-server-config.js`
--- 15-smart-city-energy-trade/referans/decentralized-energy-trading/test-contracts.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-for %%i in (.\\test\\contracts\\*.test.js) do call set "filelist=%%filelist%% %%i"
-yarn truffle test %filelist% %*
